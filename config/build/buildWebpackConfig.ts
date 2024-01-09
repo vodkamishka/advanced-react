0e7a19404dc15fc88fa0213ca6cae513b471d6f6
deynega <deynega.a@2wtrade.com>
2024-01-09 13:30:01 +0700

    Декомпозиция конфига вебпака

---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,27 @@
+import {BuildOptions} from "./types/config";
+import webpack from 'webpack';
+import path from "path";
+import {buildPlugins} from "./buildPlugins";
+import {buildLoaders} from "./buildLoaders";
+import {buildResolvers} from "./buildResolvers";
+
+export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
+
+    const {paths, mode } = options;
+
+    return {
+        mode,
+        entry: paths.entry,
+        output: {
+            filename: 'main.js',
+            path: paths.build,
+            clean: true,
+        },
+        plugins: buildPlugins(options),
+        module: {
+            rules: buildLoaders(),
+        },
+        resolve: buildResolvers(),
+    }
+
+}
