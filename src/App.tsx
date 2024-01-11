78f8a85c8d1dff2a2eb851a1795ef4d3ee9eaa45
deynega <deynega.a@2wtrade.com>
2024-01-11 13:38:15 +0700

    Добавил темы и использовал контекст

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
-import { Suspense } from 'react';
-import './index.scss';
+import {Suspense, useContext, useState} from 'react';
+import './styles/index.scss';
 import {Route, Routes, Link} from 'react-router-dom';
-import AboutPage from "./pages/AboutPage/AboutPage";
-import MainPage from "./pages/MainPage/MainPage";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
+import {Theme, ThemeContext} from "./theme/ThemeContext";
 
 function App() {
+    const {theme, setTheme} = useContext(ThemeContext);
+
+
+    const toggleTheme = () => {
+        setTheme(theme === Theme.DARK ? Theme.LIGHT : Theme.DARK);
+    }
+
+
     return (
-        <div className='app'>
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>Toggle</button>
             <Link to={"/"}>Mane page</Link>
             <Link to={"about"}>About page</Link>
             <Suspense fallback={<div>Loading...</div>}>
---
+++ src/theme/ThemeProvider.tsx
@@ -0,0 +1,26 @@
+import React, {FC, useMemo, useState} from 'react';
+import {LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext} from "./ThemeContext";
+
+const defaultTheme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme || Theme.LIGHT;
+
+interface ThemeProvider {
+    children: React.ReactNode;
+}
+
+const  ThemeProvider: FC<ThemeProvider> = ({children}) => {
+    const [theme, setTheme] = useState<Theme>(defaultTheme);
+
+    const defaultProps = useMemo(() => ({
+        theme,
+        setTheme,
+    }), [theme])
+
+
+    return (
+        <ThemeContext.Provider value={defaultProps}>
+            {children}
+        </ThemeContext.Provider>
+    );
+}
+
+export default ThemeProvider;
