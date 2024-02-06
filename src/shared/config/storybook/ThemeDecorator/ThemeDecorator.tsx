528ac0bbbd945729904ee566d12721669ea6c395
deynega <deynega.a@2wtrade.com>
2024-02-06 16:58:05 +0700

    Github actions

--- src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
+++ src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
@@ -1,6 +1,4 @@
 import '../../../../app/styles/index.scss';
-import type { Decorator } from '@storybook/react';
-import { type Theme } from 'app/providers/ThemeProvider';
 
 // eslint-disable-next-line react/display-name
 // export const ThemeDecorator: Decorator = (theme: Theme) => (Story) => (
