5a9e2f08936d9c5e727a021aaa330f6264741acc
deynega <deynega.a@2wtrade.com>
2024-02-11 22:57:28 +0700

    Github actions

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,6 +1,7 @@
 import type webpack from 'webpack';
 import { type BuildOptions } from './types/config';
 import { buildCssLoader } from './loaders/buildCssLoader';
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
 export function buildLoaders ({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     const svgLoader = {
@@ -24,7 +25,31 @@ export function buildLoaders ({ isDev }: BuildOptions): webpack.RuleSetRule[] {
         ]
     };
 
-    const cssLoader = buildCssLoader(isDev);
+    const cssLoader = {
+        test: /\.s[ac]ss$/i,
+        use: [
+            // 'style-loader' - Creates `style` nodes from JS strings
+            // MiniCssExtractPlugin.loader - Separate js and css
+            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+            // Translates CSS into CommonJS
+            {
+                loader: 'css-loader',
+                options: {
+                    modules: {
+                        // модульная система css применятеся только к файлам с расширением '.module.'
+                        auto: (resPath: string) => Boolean(resPath.includes('.module.')),
+                        // формирование названия класса в зависсимости от mode
+                        localIdentName: isDev
+                            ? '[path][name]__[local]--[hash:base64:5]'
+                            : '[hash:base64:5]'
+                    }
+                }
+            },
+            // Compiles Sass to CSS
+            'sass-loader'
+        ],
+        exclude: /node_modules/
+    };
 
     return [
         fileLoader,
