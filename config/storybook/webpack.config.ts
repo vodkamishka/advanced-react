462605419783c11fa3d30747a3fa3b33db4840fa
deynega <deynega.a@2wtrade.com>
2024-02-12 22:35:28 +0700

    Github actions

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -1,7 +1,8 @@
 import type webpack from 'webpack';
 import { type BuildPaths } from '../build/types/config';
 import path from 'path';
-import { buildCssLoader } from '../build/loaders/buildCssLoader';
+// import { buildCssLoader } from '../build/loaders/buildCssLoader';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 
 export default ({ config }: { config: webpack.Configuration }) => {
     const paths: BuildPaths = {
@@ -11,10 +12,38 @@ export default ({ config }: { config: webpack.Configuration }) => {
         src: path.resolve(__dirname, '..', '..', 'src')
     };
 
+    const isDev = true;
+
     config.resolve.modules.push(paths.src);
     config.resolve.extensions.push('.ts', '.tsx');
 
-    config.module.rules.push(buildCssLoader(true));
+    config.module.rules.push(
+        {
+            test: /\.s[ac]ss$/i,
+            use: [
+                // 'style-loader' - Creates `style` nodes from JS strings
+                // MiniCssExtractPlugin.loader - Separate js and css
+                isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+                // Translates CSS into CommonJS
+                {
+                    loader: 'css-loader',
+                    options: {
+                        modules: {
+                            // модульная система css применятеся только к файлам с расширением '.module.'
+                            auto: (resPath: string) => Boolean(resPath.includes('.module.')),
+                            // формирование названия класса в зависсимости от mode
+                            localIdentName: isDev
+                                ? '[path][name]__[local]--[hash:base64:5]'
+                                : '[hash:base64:5]'
+                        }
+                    }
+                },
+                // Compiles Sass to CSS
+                'sass-loader'
+            ],
+            exclude: /node_modules/
+        }
+    );
 
     return config;
 };
