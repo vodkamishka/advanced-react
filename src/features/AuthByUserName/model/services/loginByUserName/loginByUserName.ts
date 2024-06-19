fec78c4988a4b272ffd6263272a51e10cc0d0723
deynega <deynega.a@2wtrade.com>
2024-06-19 17:18:39 +0700

    Тестирование фичи authByUsername. TestAsyncThunk

---
+++ src/features/AuthByUserName/model/services/loginByUserName/loginByUserName.ts
@@ -0,0 +1,33 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { type User, userActions } from 'entities/User';
+import i18n from 'i18next';
+import { USER_LOCALSTORAGE_KEY } from 'shared/const/localStorage';
+
+export interface LoginByUserNameProps {
+    username: string;
+    password: string;
+}
+
+export const loginByUserName = createAsyncThunk<User, LoginByUserNameProps>(
+    'login/loginByUserName',
+    async (authData: LoginByUserNameProps, thunkAPI) => {
+        try {
+            const response = await axios.post('http://localhost:8000/login', authData);
+
+            if (!response.data) {
+                throw new Error();
+            }
+
+            localStorage.setItem(USER_LOCALSTORAGE_KEY, JSON.stringify(response.data));
+            thunkAPI.dispatch(userActions.setAuthData(response.data));
+
+            console.log('response.data', response.data);
+
+            return response.data;
+        } catch (e) {
+            console.log(e);
+            return thunkAPI.rejectWithValue('error');
+        }
+    }
+);
